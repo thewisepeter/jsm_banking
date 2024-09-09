a45357cac9e7d362106f3e76dd5f20eb90860cf3
thewisepeter <rempeterwisdom@gmail.com>
2024-09-09 13:36:09 +0300

    Implement Bank Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,40 @@
-import HeaderBox from '@/components/ui/HeaderBox'
+import HeaderBox from '@/components/ui/HeaderBox';
+import RightSidebar from '@/components/ui/RightSidebar';
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Wisdom' };
+  const loggedIn = {
+    firstName: 'Peter',
+    lastName: 'Wisdom',
+    email: 'peterwisdom@gmail.com',
+  };
   return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='home-header'>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
           <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || 'Guest'}
-          subtext="Access and manage your account and transactions efficiently"
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently"
           />
 
           <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={5000.65}
-          
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={5000.65}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1723.5 }, { currentBalance: 50000 }]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- components/ui/AnimatedCounter.tsx
+++ components/ui/AnimatedCounter.tsx
@@ -1,17 +1,12 @@
 'use client';
-import CountUp from "react-countup"
+import CountUp from 'react-countup';
 
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
     <div className="w-full">
-        <CountUp
-        decimals={2}
-        decimal=","
-        prefix="$"
-        end={amount}
-        />
+      <CountUp decimals={2} decimal="," prefix="$" end={amount} />
     </div>
-  )
-}
+  );
+};
 
-export default AnimatedCounter
+export default AnimatedCounter;
