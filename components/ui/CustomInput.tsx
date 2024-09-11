6e986511aff6cd1e41158dfe0754c8bdddeb388c
thewisepeter <rempeterwisdom@gmail.com>
2024-09-11 14:56:17 +0300

    implement reusable input form

---
+++ components/ui/CustomInput.tsx
@@ -0,0 +1,37 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const CustomInput = ({ form, name, label, placeholder, type }) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={type}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
